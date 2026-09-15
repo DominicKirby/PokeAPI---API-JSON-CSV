a57fdd8bcf602ded118b5eb671b7fd11fd9bb796
Dominic Kirby <dkirby@spartaglobal.com>
2026-09-15 16:14:37 +0100

    Final update and commenting code

--- Handling_Data.py
+++ Handling_Data.py
@@ -3,13 +3,31 @@ import json
 import csv
 
 def get_information(pokemon: str):
+    """
+    Requests the information about a given string of a pokemon
+    """
+
+    # Initial URL
     url = "https://pokeapi.co/api/v2/pokemon/"
+
+    # Building new URL
     request = requests.get((url + pokemon))
+
+    # Requesting the information from the API
     key_information = json.loads(request.text)
     return key_information
 
+
+
 def key_information_parsing(pokemon: str):
+    """
+    Parses the key pokemon information from the JSON recieved from the get_information functions, takes json input and
+    gives output including id, name, base_experience, height, is_default, order, weight, and names of abilities
+    """
+    # Using API to get pokemon information
     pokemon_info = get_information(pokemon)
+
+    # Parsing key data
     id = pokemon_info["id"]
     name = pokemon_info["name"].capitalize()
     base_experience = pokemon_info["base_experience"]
@@ -18,22 +36,39 @@ def key_information_parsing(pokemon: str):
     order = pokemon_info["order"]
     weight = pokemon_info["weight"]
 
+
+    # Going into the abilities sub dictionary and retrieving the names to add to an abilities list in a unique format
     abilities = pokemon_info["abilities"]
     all_abilities = ""
     for ability in abilities:
+
+        # Retrieving the name
         ability_name = ability["ability"]["name"]
+
+        # Reformating the name
         ability_name = ability_name.capitalize()
         ability_name = ability_name.replace("-", " ")
 
+        # Adding the abilities to a set list in the format of Blast, Attack move, etc.
         if all_abilities == "":
             all_abilities = ability_name
         else:
             all_abilities += ", " + ability_name
 
+    # Returining all key information
     return [id, name, base_experience, height, is_default, order, weight, all_abilities]
 
+
+
 def create_csv(pokemon: list):
+    """
+    Creating csv from the data we have parsed previously
+    """
+
+    # Headers of the csv
     header = ["id", "name", "base_experience", "height", "is_default", "order", "weight", "abilities"]
+
+    # Adding the new row to the csv for that pokemon using the previous function
     with open("pokemon.csv", "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(header)
--- main.py
+++ main.py
@@ -1,46 +1,64 @@
 from Handling_Data import *
 
-
+# Setting the application to run until it is told to stop
 application = True
 
+# Empty list to be added to later
 pokemon_list = []
 
-print("\nThis is an interface to create the pokemon csv list of the key information per pokemon you input. Please input values of a format: a, b, c, d. Type N to exit and type Clear to clear the list ")
+# User information
+print("\nThis is an interface to create the pokemon csv list of the key information per pokemon you input.\nPlease input values of a format: a, b, c, d.\nType N to exit, Clear to clear the list and default for a general set.")
 
 while application:
+    # Creating an old list to add to the pre-existing pokemon list
     old_list = input("\nWhich pokemon do you want to see? ")
 
+    # Setting a default value for if you user wants a general output
     if old_list == "default":
-        old_list = "charmander, charmeleon, charizard, squirtle, wartortle, blastoise, bulbasaur, ivysaur, venusaur"
+        old_list = "bulbasaur, ivysaur, venusaur, charmander, charmeleon, charizard, squirtle, wartortle, blastoise "
 
+    # Trying to add the pokemon to the csv file and if it fails then determining this is an invalid input
     try:
+        # Separating the input into an iterable list
         old_list = old_list.split(",")
+
+        # Creating a list to give reference to the user about what has been added to the list
         pokemon_added = []
 
+        # Giving the user the ability to exit
         if old_list[0].lower() == "n":
             application = False
 
+        # Giving the user the ability to clear the current csv file
         elif old_list[0].lower() == "clear":
             pokemon_list.clear()
             print("List cleared")
 
+        # Adding the new pokemon to the pre-existing list and csv file
         else:
             for pokemon in old_list:
                 pokemon = pokemon.strip()
                 try:
+                    # Checking whether its already in the list and giving a message if so
                     if pokemon.strip() in pokemon_list:
                         print(f"{pokemon.capitalize()} is already in the csv. ")
+
+                    # Adding the pokemon to the csv
                     else:
                         pokemon_list.append(pokemon)
                         create_csv(pokemon_list)
                         pokemon_added.append(pokemon)
                 except KeyError:
+                    # If the pokemon was not found in the database giving a real return and leaving the application
                     print(f"The pokemon {pokemon} is not in the database. Please give valid input.")
                     application = False
 
+            # Giving a unique output for no pokemon added
             if len(pokemon_added) == 0:
                 print("No pokemon were added to the csv")
+            # General answer with the pokemon added in this iteration
             else:
                 print("The following pokemon have been added to the csv file:", pokemon_added)
+    # Error message
     except:
         print("Please give a valid input")
